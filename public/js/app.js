c7f9a22fd3a70f8107c157d450d08f958e850863
Teddy Otieno <teddy.asola@andela.com>
2016-11-18 20:09:28 +0300

    Initalize angular app with needed dependencies

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
-alert('Daylight');
+'use strict';
+(function() {
+    var entertainmentAtlas = angular.module('entertainmentAtlas', ['ui.router', 'ngAria', 'ngAnimate', 'ngMaterial']);
+
+    entertainmentAtlas.config(['$stateProvider', '$httpProvider', '$urlRouterProvider', '$locationProvider', function($stateProvider, $httpProvider, $urlRouterProvider, $locationProvider) {
+
+        $urlRouterProvider.otherwise('/');
+
+        $stateProvider
+            .state('home', {
+                url: '/',
+                templateUrl: 'views/home.html'
+            });
+
+        // Remove hash from url paths
+        $locationProvider.html5Mode(true);
+
+    }]);
+}());
