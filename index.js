8f0ea0cc5be724b693415476380f58aba60c5a8d
Teddy Otieno <teddy.asola@andela.com>
2016-11-18 14:50:08 +0300

    Load requried dependencies, serve static files, listen on a port, create basic route

---
+++ index.js
@@ -0,0 +1,22 @@
+'use strict';
+// LOAD REQUIRED DEPENDENCIES //
+const express = require('express'),
+    path = require('path'),
+    envVar = require('dotenv').config(),
+    app = express();
+
+
+// SERVE STATIC FILES
+express.static(path.join(__dirname + '/public'));
+
+// BASIC ROUTE
+app.get('/', function(req, res) {
+    res.sendFile('index.html', {
+        root: './public'
+    });
+});
+
+// START THE SERVER
+app.listen(envVar.PORT, function() {
+    console.log('Server running on PORT: ' + envVar.PORT);
+});
