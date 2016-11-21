d653956c694c1ad1945dfe08c9bb8dfecf038f38
Teddy Otieno <teddy.asola@andela.com>
2016-11-21 22:21:42 +0300

    Add Markers on page

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -1,8 +1,25 @@
 'use strict';
 angular.module('entertainmentAtlas')
-    .controller('HomeCtrl', function($scope) {
-        var cartodb = window.cartodb;
-        window.onload = function() {
-            cartodb.createVis('map', 'http://documentation.carto.com/api/v2/viz/2b13c956-e7c1-11e2-806b-5404a6a683d5/viz.json');
-        };
+    .controller('HomeCtrl', function($scope, DataService) {
+        var L = window.L;
+        var map = new L.Map('map').setView([41.8914184, -87.6116459], 11);
+        L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png', {
+            attribution: 'Positron'
+        }).addTo(map);
+        var redMarker = new L.Icon({
+            iconSize: [20, 32],
+            iconAnchor: [13, 27],
+            popupAnchor: [13, -30],
+            iconUrl: '../images/marker.svg',
+        });
+        DataService.fetchData().then(function(data) {
+            $scope.data = data.data.feed.entry;
+            for (var i = 0; i < $scope.data.length; i++) {
+                angular.forEach($scope.data[i], function(value, key) {
+                    L.marker([$scope.data[i].gsx$latitude.$t, $scope.data[i].gsx$longitude.$t], { icon: redMarker }).addTo(map);
+                });
+            }
+        }, function(err) {
+            console.log('There was an error: ' + err);
+        });
     });
--- public/js/services/data-request-service.js
+++ public/js/services/data-request-service.js
@@ -2,5 +2,18 @@
 angular.module('entertainmentAtlas')
     .factory('DataService', function($http, $q) {
         var service = {};
+
+        service.fetchData = function() {
+            var deferred = $q.defer();
+            $http({
+                method: 'GET',
+                url: 'https://spreadsheets.google.com/feeds/list/1TEkUmfZqqdaHSJ1NXz4Dsx5H5sHFoWLF_PnhFNXkOqc/od6/public/values?alt=json'
+            }).then(function success(resp) {
+                deferred.resolve(resp);
+            }, function error(err) {
+                deferred.reject(err);
+            });
+            return deferred.promise;
+        };
         return service;
-    });
+    })
