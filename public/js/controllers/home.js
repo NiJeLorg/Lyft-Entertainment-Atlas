0604c9dc2a84f30fcb12de321e42961a3d7e34a3
JD Godchaux <jd@nijel.org>
2016-12-02 11:44:25 -0500

    tripling padding

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -90,7 +90,7 @@ angular.module('entertainmentAtlas')
                         map.flyToBounds(bounds, {
                             animate: true,
                             duration: 2,
-                            paddingBottomRight: [200, 0], 
+                            paddingBottomRight: [600, 0], 
                         });
                         // map.flyTo([lat, lng], 15, {
                         //     animate: true,
