5a852ec2bdb665dd55e15da9e38eba5c3f90041b
JD Godchaux <jd@nijel.org>
2016-12-19 20:08:26 -0500

    passing lat lng to tiny page to make iphones work

--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -70,15 +70,15 @@ angular.module('entertainmentAtlas')
         $scope.orderLyft = function() {
             if ($('body').width() < 768) {
                 var url = 'lyft://ridetype?id=lyft&partner=4ujGa8RbFc5n&destination[latitude]=' + $scope.selectedLocation.gsx$latitude.$t + '&destination[longitude]=' + $scope.selectedLocation.gsx$longitude.$t;
+                var iphoneurl = 'https://qa-visualizations.dnainfo.com/lyft_test/?lat=' + $scope.selectedLocation.gsx$latitude.$t + '&lng=' + $scope.selectedLocation.gsx$longitude.$t;
                 if (!navigator.userAgent.toLowerCase().indexOf("iphone")) {
                     deeplink.open(url);
                 } else {
-                    window.open('https://qa-visualizations.dnainfo.com/lyft_test/');
-                    // try {
-                    //     window.open(url, '_blank');
-                    // } catch (e) {
-                    //     window.open('https://itunes.apple.com/us/app/lyft-on-demand-ridesharing/id529379082?mt=8', '_blank');
-                    // }
+                    try {
+                        window.open(iphoneurl, '_blank');
+                    } catch (e) {
+                        window.open('https://itunes.apple.com/us/app/lyft-on-demand-ridesharing/id529379082?mt=8', '_blank');
+                    }
                 }
 
             } else {
